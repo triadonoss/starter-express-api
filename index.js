62d4bce0446f61e3e9dca67f8d143f71bb0a1f21
triadonoss <85204936+triadonoss@users.noreply.github.com>
2022-12-21 22:42:04 +0800

    dependencies

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const PORT = process.env.PORT || 3500
+const cors = require('cors')
+
+const configuration = new Configuration({
+    organization: "org-Mlt1HHrtFYtlPXokEqrcVPAi",
+    apiKey: process.env.OPEN_AI_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (true) {
+            callback(null, true)
+        }
+    },
+    optionsSuccessStatus: 200
+}))
+app.use(express.json())
+app.post('/', async (req,res)=>{
+    const {promps} = req.body
+
+    const response = await openai.createCompletion({
+        'model': "text-davinci-003",
+        'prompt': promps,
+        'max_tokens': 100,
+        'temperature': 0
+    }) 
+
+    res.send(response.data?.choices)
 })
-app.listen(process.env.PORT || 3000)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
